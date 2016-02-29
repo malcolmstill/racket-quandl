63ba7ca27de8b32079bfaa693806bf10671b639b
Malcolm <malcolm.still@gmail.com>
2016-02-29 18:40:34 +0000

    Added further parameters

--- main.rkt
+++ main.rkt
@@ -30,16 +30,29 @@
                0
                0)))
                
-(define (get code #:collapse [collapse ""])
+(define (get code
+             #:rows [rows ""]
+             #:column-index [column-index ""]
+             #:collapse [collapse ""]
+             #:start-date [start-date ""]
+             #:end-date [end-date ""]
+             #:order [order ""]
+             #:transform [transform ""])
   (let ([query (read-json
-               (get-pure-port
-                (string->url
-                 (string-append dataurl code ".json?"
-                                (string-join
-                                 (list
-                                  (param->string "auth_token" authcode)
-                                  (param->string "collapse" collapse))
-                                 "&")))))])
+                (get-pure-port
+                 (string->url
+                  (string-append dataurl code ".json?"
+                                 (string-join
+                                  (list
+                                   (param->string "auth_token" authcode)
+                                   (param->string "rows" rows)
+                                   (param->string "column_index" column-index)
+                                   (param->string "collapse" collapse)
+                                   (param->string "start_date" start-date)
+                                   (param->string "end_date" end-date)
+                                   (param->string "order" order)
+                                   (param->string "transform" order))
+                                  "&")))))])
     (if (hash-has-key? query 'quandl_error)
         (error (hash-ref (hash-ref query 'quandl_error) 'message))
         (let ([data (hash-ref (hash-ref query 'dataset) 'data)])
